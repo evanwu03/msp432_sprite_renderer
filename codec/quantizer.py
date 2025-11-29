fd7a40f9f4aaccfd0a958276996aacdf477eca28
Evan Wu <ewu43951@gmail.com>
2025-11-28 23:55:37 -0600

    fixed error in split_bucket and refactored palette_bgr24_to_bgr565 using numpy bitwise operations instead of iteration

--- codec/quantizer.py
+++ codec/quantizer.py
@@ -4,33 +4,29 @@ import numpy as np
 
 def palette_bgr24_to_bgr565(palette24: np.ndarray) -> np.ndarray:
 
-    palette565 = []
-    for px in palette24:
-        B = (px >> 16) & 0xFF
-        G = (px >> 8)  & 0xFF
-        R =  px        & 0xFF
-
-        r5 = R >> 3
-        g6 = G >> 2
-        b5 = B >> 3
-
-        rgb565 = (r5 << 11) | (g6 << 5) | b5
-        palette565.append(rgb565)
-    return palette565
+    B = (palette24 >> 16) & 0xFF
+    G = (palette24 >> 8)  & 0xFF
+    R =  palette24         & 0xff
+    
+    B5 = B >> 3
+    G6 = G >> 2
+    R5 = R >> 3
+   
+    bgr565 = (B5 << 11) | (G6 << 5) | R5
+    return bgr565
+    
 
 
-
-
-
-def generate_palette(bucket, num_colors=256):
-
+def generate_palette(bucket: np.ndarray, num_colors=256)  -> np.ndarray:
+ 
     target_depth = int(np.log2(num_colors)) 
     assert 2**target_depth == num_colors # "Num colors must be a power of 2!"
+    palette = median_cut_recurse(bucket, 0, target_depth)
+    return np.array(palette)
 
-    return median_cut_recurse(bucket, 0, target_depth)
 
 
-def median_cut_recurse(bucket, level, target_depth):
+def median_cut_recurse(bucket: np.ndarray, level: int, target_depth: int) -> np.ndarray:
 
     if level == target_depth:
         return [averageColor(bucket)]
@@ -41,6 +37,9 @@ def median_cut_recurse(bucket, level, target_depth):
     right_colors = median_cut_recurse(upper_palette, level+1, target_depth)
 
     return left_colors + right_colors
+
+
+
 def split_bucket(pixels: np.ndarray) -> np.ndarray: 
 
     key = None
@@ -51,7 +50,7 @@ def split_bucket(pixels: np.ndarray) -> np.ndarray:
 
     b_range = B.max() - B.min()
     g_range = G.max() - G.min()
-    r_range = G.max() - R.min()
+    r_range = R.max() - R.min()
 
     if b_range >= g_range and b_range >= r_range:
         key = (pixels >> 16) & 0xFF     # blue
