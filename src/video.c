3baea86a28d3d5175e76a66cea5dbd85a1c6dd92
Evan Wu <ewu43951@gmail.com>
2025-12-21 17:51:14 -0600

    video length should be unsigned long

---
+++ src/video.c
@@ -0,0 +1,11 @@
+
+#include "video.h"
+
+const uint8_t video_stream[] = {
+
+    #include "video_data.inc"
+};
+
+
+unsigned long video_len = sizeof(video_stream);
+
---
+++ src/video.h
@@ -0,0 +1,12 @@
+
+
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include <stdint.h>
+
+extern const uint8_t video_stream[];
+
+extern unsigned long video_len;
+
+#endif
