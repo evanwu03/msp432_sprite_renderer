f7d07228df361b677a0a53c52e7ea8447e4018e5
Evan Wu <ewu43951@gmail.com>
2025-12-27 11:53:53 -0600

    set up CPPUtest environment

---
+++ tests/tests.cpp
@@ -0,0 +1,31 @@
+
+
+// Headers 
+#include "CppUTest/TestHarness.h" 
+
+
+
+TEST_GROUP(decoderTests) {
+
+    void setup() { 
+
+
+    }
+
+    void teardown() { 
+
+
+    }
+
+
+};
+
+
+
+TEST(decoderTests, parseStreamHeaderSuccess)  {
+
+
+    FAIL("Nothing is here");
+
+}
+
